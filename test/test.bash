e0d51512c7541034c283d9c31d02c0fe334ceb5b
Natsuhi Shimada <gumball7229@gmail.com>
2025-12-30 20:07:51 +0900

    test5

--- test/test.bash
+++ test/test.bash
@@ -2,16 +2,12 @@
 # SPDX-FileCopyrightText: 2025 Natsuhi Shimada
 # SPDX-License-Identifier: BSD-3-Clause
 
-dir=~
-[ "$1" != "" ] && dir="$1"
+dir=$1
+[ "$dir" == "" ] && dir=$GITHUB_WORKSPACE
 
-cd $dir/ros2_ws || exit 1
-
+cd $dir/src/mypkg || exit 1
 colcon build --symlink-install || exit 1
-
 . install/setup.bash
-
 timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log 2>&1
-
 grep 'Listen: 10' /tmp/mypkg.log || exit 1
 
