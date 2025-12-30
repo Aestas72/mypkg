855adddcb50c3d0e43210f54df9681f2f16268b0
Natsuhi Shimada <gumball7229@gmail.com>
2025-12-30 19:56:58 +0900

    test3

--- test/test.bash
+++ test/test.bash
@@ -2,18 +2,16 @@
 # SPDX-FileCopyrightText: 2025 Natsuhi Shimada
 # SPDX-License-Identifier: BSD-3-Clause
 
-set -e
-
-source /opt/ros/humble/setup.bash
-
 dir=~
 [ "$1" != "" ] && dir="$1"
 
-cd $dir/ros2_ws
+cd $dir/ros2_ws || exit 1
 
-colcon build --symlink-install
+colcon build --symlink-install || exit 1
 
-timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log || true
+source install/setup.bash
 
-grep 'Listen: 10' /tmp/mypkg.log
+timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log 2>&1
+
+grep 'Listen: 10' /tmp/mypkg.log || exit 1
 
